74613c639ec03377c99b8413129a50201fbaefe2
Bartosz Niewiadomski <bartekniew2@gmail.com>
2022-10-20 15:50:36 +0200

    zadanie 1 and zadanie 2

--- apkWWW/aplikacja1/models.py
+++ apkWWW/aplikacja1/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+# kod z oficjalnej dokumentacji Django
+class Osoba(models.Model):
+    name = models.CharField(max_length=60)
+    surname = models.CharField(max_length=60)
+    Miesiace=(
+        ('1','Styczeń'),
+        ('2','Luty'),
+        ('3','Marzec'),
+        ('4','Kwiecień'),
+        ('5','Maj'),
+        ('6','Czerwiec'),
+        ('7','Lipiec'),
+        ('8','Sierpień'),
+        ('9','Wrzesień'),
+        ('10','Październik'),
+        ('11','Listopad'),
+        ('12','Grudzień'),
+    )
+    miesiac_urodzenia = models.CharField(max_length=60, choices=Miesiace)
+
