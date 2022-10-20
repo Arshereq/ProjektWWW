03970ce777ae2cffe61008729da778233d75fcad
Bartosz Niewiadomski <bartekniew2@gmail.com>
2022-10-20 16:21:01 +0200

    zadanie 4

--- apkWWW/aplikacja1/models.py
+++ apkWWW/aplikacja1/models.py
@@ -5,19 +5,22 @@ from django.db import models
 class Osoba(models.Model):
     name = models.CharField(max_length=60)
     surname = models.CharField(max_length=60)
-    Miesiace=(
-        ('1','Styczeń'),
-        ('2','Luty'),
-        ('3','Marzec'),
-        ('4','Kwiecień'),
-        ('5','Maj'),
-        ('6','Czerwiec'),
-        ('7','Lipiec'),
-        ('8','Sierpień'),
-        ('9','Wrzesień'),
-        ('10','Październik'),
-        ('11','Listopad'),
-        ('12','Grudzień'),
-    )
-    miesiac_urodzenia = models.CharField(max_length=60, choices=Miesiace)
+    class Miesiace(models.IntegerChoices):
+        Styczen = 1
+        Luty = 2
+        Marzec =3
+        Kwiecien = 4
+        Maj = 5
+        Czerwiec = 6
+        Lipiec = 7
+        Sierpien =8
+        Wrzesien =9
+        Pazdziernik =10
+        Listopad =11
+        Grudzien = 12
+    #NIE DZIALA TA WERSJA
+    #miesiac = models.IntegerChoices('miesiac','Styczeń Luty Marzec Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień')
+    #miesiac.choices
+    #miesiac_urodzenia = models.CharField(max_length=60, choices=Miesiace)
+    miesiac_urodzenia = models.IntegerField(choices=Miesiace.choices)
     data_dodania = models.DateField(auto_now=True)
